1e6f06669d820e1e23667353f4e91405c45b03ca
Tom0218 <mark29737148mark@gmail.com>
2023-10-25 09:11:38 +0800

    loginandregister finsish

---
+++ src/components/Register.vue
@@ -0,0 +1,131 @@
+<script>
+export default{
+    data(){
+        return{
+            Email:"",
+            Password:"",
+            showText:"",
+            outputArr:"",
+            userData:[],
+
+        }
+    },
+
+    methods:{
+
+        RegisterBtn(){
+            let userinfo={
+                account:this.Email,
+                pwd:this.Password
+            }
+            this.userData.push(userinfo)
+            let existingUserData = JSON.parse(localStorage.getItem("user")) || [];
+            existingUserData.push(userinfo);
+            localStorage.setItem("user", JSON.stringify(existingUserData));
+
+
+            // if(JSON.parse(localStorage.getItem("user")) == null){//若為第一次使用localstorage
+            //     localStorage.setItem("user",JSON.stringify(userData));
+            //     showText = "註冊成功";
+
+            // for(let i = 0;i<)
+            // this.outputArr = JSON.parse(localStorage.getItem("user"));
+            //     // console.log(this.outputArr[0].account)
+            //     for(let i = 0;i<)
+            },
+        LogIn(){
+            let x = 0
+            this.$emit('changepage',x)
+        }
+    }
+}
+
+</script>
+
+<template>
+<div class="body">
+    <div class="page">
+            <div class="mainArea">
+                <h1>Register</h1>
+                <div class="emailbox">
+                    <p >email</p>
+                    <input type="text" v-model="Email">
+                    <p></p>
+                </div>
+                <div class="namebox">
+                    <p>name</p>
+                    <input >
+                </div>
+                <div class="passwordbox">
+                    <p>password</p>
+                    <input type="text" v-model="Password" >
+                </div>
+                <div class="repeatpasswordbox">
+                    <p >Repeatpassword</p>
+                    <input id="reppwd">
+                </div>
+                <p>{{ showText }}</p>
+                <div class="Signupbtnbox">
+                    <button @click="RegisterBtn">Register</button>
+                </div>
+                <button type="button" @click="LogIn()">logIn</button>
+            </div>
+        </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.body{
+    height: 100%;
+    margin: 0;
+    background-color:gray;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+p{
+    font-size: 16pt;
+    margin: 10px 0;
+    font-weight: bold;
+}
+
+input{
+    height: 25px;
+    width: 300px;
+    margin: 0;
+    padding: 0;
+}
+
+a{
+    color: black;
+    text-decoration: none;
+    display: flex;
+    justify-content: end;
+    font-weight: bold;
+}
+
+h1{
+    text-align: center;
+    margin-bottom: 30px;
+}
+
+.page{
+    width: 500px;
+    height: 500px;
+    display: flex;
+    justify-content: center;
+    .mainArea{
+        flex-direction: column;
+    }
+    
+}
+
+#end{
+    text-align: center;
+}
+.Signupbtnbox{
+    text-align: center;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,157 @@
-<script >
+<script>
+import Register from "../components/Register.vue"; 
+
+export default{
+        data(){
+            return{
+                getData :"",
+                Email : "",
+                Password : "",
+                check1:"",
+                userData :{},
+                UserDatalength : "",
+                page:0
+
+            }
+        },
+        components:{
+            Register
+        },
+        methods:{
+            LogIn(){
+                this.getData = localStorage.getItem('user');
+                this.userData = JSON.parse( this.getData );
+                this.UserDatalength = this.userData.length;
+                for(let i = 0; i <  this.UserDatalength+1 ;i++){
+                    if (this.Email == this.userData[i].account && this.Password == this.userData[i].pwd){
+
+                        console.log("有這個帳號跟密碼");
+                        this.check1 = "登入成功";  
+                        this.Email = ""
+                        this.Password=""
+                        this.$router.push('/ExpenseTracker');
+
+                        return
+                        
+                    }else{
+                        this.check1 = "帳號或密碼錯誤"
+                    }
+                }
+            },
+            changePage(x){
+                this.page = x
+            },
+            GoRegister(){
+                this.page +=1
+            }
+        }
+}
 </script>
 
 <template>
+<div class="body" >
+    <div class="mainArea">
+            <div class="Gamebox" v-if="this.page==0">
+                <h1>login Page</h1>
+                <p>Email</p>
+                <input type="text" v-model="Email">
+                <p>Password</p>
+                <input type="text" v-model="Password">
+                <div class="line-5">
+                    <input type="checkbox" class="chekBox">
+                    <small>Keep login</small>
+                    <button type="button" @click="GoRegister()">Register</button>
+                </div>
+                <button type="button" class="loginbtn" @click="LogIn()">Log In</button>
+                <div class="showText"><p>{{ check1 }}</p></div>
+                
+            </div>
+            <div v-else-if="page ==1">
+                <Register @changepage ="changePage"/>
+            </div>
+        </div>
+        </div>
 
-  
 </template>
+
+<style lang="scss" scoped>
+.body{
+    height: 100vh;
+    margin: 0;
+    background-color: gray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+p{
+    font-size: 16pt;
+    margin: 10px 0;
+    font-weight: bold;
+}
+h1{
+    text-align: center;
+    margin-bottom: 30px;
+}
+
+.mainArea{
+    width: 500px;
+    display: flex;
+    justify-content: center;
+    .Gamebox{
+        width: 60%;
+        input{
+            height: 25px;
+            width: 100%;
+            margin: 0;
+            padding: 2px;
+
+        }
+        .line-5{
+            width: 100%;
+            display: flex;
+            align-items: center;
+            position: relative;
+            margin-top: 20px;
+            a{
+                width: 80px;
+                text-align: center;
+                text-decoration: none;
+                background-color: white;
+                color: black;
+                font-size: 16pt;
+                position: absolute;
+                right: 0;
+            }
+        }       
+        .chekBox{
+            margin: 2px;
+            height: 20px;
+            width: 20px;
+        }
+        button{
+            width: 40%;
+            height: 40px;
+            border: none;
+            font-size: 16pt;
+            margin-left: 29%;
+        }
+        #check1{
+            text-align: center;
+        }
+        .loginbtn{
+            margin-top: 30px;
+        }
+        small{
+            width: 50px;
+            margin-left: 5%;
+        }
+        .showText{
+            margin-left: 50px;
+        }
+        
+
+    }
+    
+}
+
+</style>
